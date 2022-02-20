25a729590913e68ca851a26cd9d0cf056f66b138
Sai Siddartha Maram <samaram@ucsc.edu>
2022-02-19 20:12:47 -0800

    debug false

--- resilience_backend/settings.py
+++ resilience_backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8(k8y!kndfhwe%7ykps+_*duvjq621i#8-7)(0g)xlw@@m05-q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
